ae953426edb2acabd333d4145651eaf69b1723fa
孙宗正 <sunzongzheng@bytedance.com>
2024-04-11 13:15:24 +0800

    feat: 更新配置

---
+++ user_config.py
@@ -0,0 +1,16 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "湖南卫视",
+    "浙江卫视",
+    "江苏卫视",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
